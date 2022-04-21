5a594efa9726b1cc851e541059baa67384d3b4c2
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-04-21 14:42:25 +0200

    add milestone 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,34 +14,48 @@ Bonus:
 */
 const app = new Vue({
   el: "#app",
-  data:{
-      locations:[
-        {
-            image: './assets/img/01.jpg',
-            title: 'Svezia',
-            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-        },
-        {
-            image: './assets/img/02.jpg',
-            title: 'Svizzera',
-            text: 'Lorem ipsum.',
-        },
-        {
-            image: './assets/img/03.jpg',
-            title: 'Gran Bretagna',
-            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-        },
-        {
-            image: './assets/img/04.jpg',
-            title: 'Germania',
-            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-        },
-        {
-            image: './assets/img/05.jpg',
-            title: 'Paradise',
-            text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-        }
+  data: {
+    locations: [
+      {
+        image: "./assets/img/01.jpg",
+        title: "Svezia",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+      },
+      {
+        image: "./assets/img/02.jpg",
+        title: "Svizzera",
+        text: "Lorem ipsum.",
+      },
+      {
+        image: "./assets/img/03.jpg",
+        title: "Gran Bretagna",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        image: "./assets/img/04.jpg",
+        title: "Germania",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.",
+      },
+      {
+        image: "./assets/img/05.jpg",
+        title: "Paradise",
+        text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.",
+      },
     ],
-    active_image:0,
-  }
+    active_image: 0,
+  },
+  methods: {
+      prev_img(){
+          if(this.active_image === 0){
+              this.active_image = this.locations.length
+          }
+          this.active_image--
+      },
+      next_img(){
+          this.active_image++
+        if(this.active_image === this.locations.length){
+            this.active_image = 0
+        }
+    }
+  },
 });
